0a67fac384c75b1810112ae238b10349062aad37
MiSaKi119 <1347443575@qq.com>
2023-11-29 20:39:05 +0800

    feat(子组件): 112
    啥也么有
    
    BREAKING CHANGE: 不多

---
+++ src/components/dzx.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  filters: {},
+  watch: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
